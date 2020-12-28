21af75683ea84bb3ef52e2dda482ffdc7548ecb0
Nacer Sebti <nacersebti.ms@gmail.com>
2020-12-28 14:07:45 -0500

    Added TODO work as comments

--- jQuery/jQuery-HTML-Elements/js/home.js
+++ jQuery/jQuery-HTML-Elements/js/home.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
-    
+
 });
+
+
+//    Center all H1 elements.
+
+
+
+//    Center all H2 elements.
+
+
+
+//    Replace the class that is on the element containing the text "Team Up!" with the class page-header.
+
+
+
+//    Change the text of "The Squad" to "Yellow Team".
+
+
+
+//    Change the background color for each team list to match the name of the team.
+
+
+
+//    Add Joseph Banks and Simon Jones to the Yellow Team list.
+
+
+
+//    Hide the element containing the text "Hide Me!!!"
+
+
+
+//    Remove the element containing the text "Bogus Contact Info" from the footer.
+
+
+
+//    Add a paragraph element containing your name and email to the footer. The text must be in Courier font and be 24 pixels in height.
