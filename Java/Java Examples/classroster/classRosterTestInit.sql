9b49cc376e775e47820541fa0fe63cef1d6ed31b
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-05 15:58:52 -0500

    class roster: db setup

---
+++ Java/Java Examples/classroster/classRosterTestInit.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS classRosterTest;
+CREATE DATABASE classRosterTest;
+
+USE classRosterTest;
+
+CREATE TABLE teacher(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    specialty VARCHAR(50)
+);
+
+CREATE TABLE student(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE course(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    teacherId INT NOT NULL,
+    FOREIGN KEY (teacherId) REFERENCES teacher(id)
+);
+
+CREATE TABLE course_student(
+    courseId INT NOT NULL,
+    studentId INT NOT NULL,
+    PRIMARY KEY(courseId, studentId),
+    FOREIGN KEY (courseId) REFERENCES course(id),
+    FOREIGN KEY (studentId) REFERENCES student(id)
+);
