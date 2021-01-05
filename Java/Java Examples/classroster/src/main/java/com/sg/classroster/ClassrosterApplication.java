5d0cf6b97c2e32c851e5a3fc6979aa0c1866e648
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-05 15:30:33 -0500

    class roster init project

---
+++ Java/Java Examples/classroster/src/main/java/com/sg/classroster/ClassrosterApplication.java
@@ -0,0 +1,13 @@
+package com.sg.classroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassrosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassrosterApplication.class, args);
+	}
+
+}
---
+++ Java/Java Examples/classroster/src/test/java/com/sg/classroster/ClassrosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.classroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassrosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
