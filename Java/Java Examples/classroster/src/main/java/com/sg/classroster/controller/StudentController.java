e99134da2a685f22d320076ae54c0ffc23e9f9f6
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-06 09:03:29 -0500

    Added students pages + controller

---
+++ Java/Java Examples/classroster/src/main/java/com/sg/classroster/controller/StudentController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.classroster.controller;
+
+import com.sg.classroster.dao.CourseDAO;
+import com.sg.classroster.dao.StudentDAO;
+import com.sg.classroster.dao.TeacherDAO;
+import com.sg.classroster.models.Student;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author nacer
+ */
+@Controller
+public class StudentController {
+    
+    @Autowired
+    TeacherDAO teacherDao;
+
+    @Autowired
+    StudentDAO studentDao;
+
+    @Autowired
+    CourseDAO courseDao;
+    
+    @GetMapping("students")
+    public String displayStudents(Model model) {
+        List<Student> students = studentDao.getAllStudents();
+        model.addAttribute("students", students);
+        return "students";
+    }
+    
+    @PostMapping("addStudent")
+    public String addStudent(String firstName, String lastName) {
+        Student student = new Student();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        studentDao.addStudent(student);
+        
+        return "redirect:/students";
+    }
+    
+    @GetMapping("deleteStudent")
+    public String deleteStudent(Integer id) {
+        studentDao.deleteStudentById(id);
+        return "redirect:/students";
+    }
+    
+    @GetMapping("editStudent")
+    public String editStudent(Integer id, Model model) {
+        Student student = studentDao.getStudentById(id);
+        model.addAttribute("student", student);
+        return "editStudent";
+    }
+    
+    @PostMapping("editStudent")
+    public String performEditStudent(Student student) {
+        studentDao.updateStudent(student);
+        return "redirect:/students";
+    }
+}
